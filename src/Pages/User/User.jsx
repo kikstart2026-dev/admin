9ff4fe4a582f56d52eaecbda778ebac20e6a0ed5
kikstart2026-dev <poulomisaha588@gmail.com>
2026-05-09 13:32:27 +0530

    --

--- src/Pages/User/User.jsx
+++ src/Pages/User/User.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
-import UserControl from '../../Component/UserControl/UserControl'
+import React, { useState } from "react";
 import styles from "./User.module.scss";
+import UserControl from "../../Component/UserControl/UserControl";
+
 export default function User() {
-    return (
-        <div className={styles.container}>
+  const [activeSection, setActiveSection] = useState(null);
 
+  const toggleSection = (section) => {
+    setActiveSection((prev) => (prev === section ? null : section));
+  };
 
-            <UserControl />
+  return (
+    <div className={styles.container}>
+      <div className={styles.section}>
+        {/* HEADER */}
+        <div
+          className={`${styles.sectionHeader} ${
+            activeSection === "user" ? styles.active : ""
+          }`}
+          onClick={() => toggleSection("user")}
+        >
+          <span>User Management Control</span>
+
+          <i
+            className={`bi ${
+              activeSection === "user"
+                ? "bi-chevron-up"
+                : "bi-chevron-down"
+            }`}
+          ></i>
         </div>
 
-    )
+        {/* BODY */}
+        {activeSection === "user" && (
+          <div className={styles.sectionBody}>
+            <UserControl />
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
