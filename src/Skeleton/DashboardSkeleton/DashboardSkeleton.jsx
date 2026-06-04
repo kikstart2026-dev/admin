67b142389591a5e3d799c7b9d3fdd076f79d2866
kikstart2026-dev <poulomisaha588@gmail.com>
2026-06-04 20:45:20 +0530

    --

--- src/Skeleton/DashboardSkeleton/DashboardSkeleton.jsx
+++ src/Skeleton/DashboardSkeleton/DashboardSkeleton.jsx
@@ -3,61 +3,50 @@ import styles from "./DashboardSkeleton.module.scss";
 
 export default function DashboardSkeleton() {
   return (
-    <div className={styles.dashboardSkeleton}>
-
+    <div className={`container-fluid mt-4 ${styles.dashboardSkeleton}`}>
+      
+      {/* Title */}
       <div className={styles.title}></div>
 
       {/* Stats Cards */}
-      <div className={styles.statsGrid}>
+      <div className="row g-4">
         {[1, 2, 3, 4].map((item) => (
-          <div key={item} className={styles.card}></div>
+          <div key={item} className="col-md-3">
+            <div className={styles.statsCard}></div>
+          </div>
         ))}
       </div>
 
       {/* Charts */}
-      <div className={styles.chartGrid}>
-
-        <div className={styles.chartCard}>
-          <div className={styles.heading}></div>
-          <div className={styles.pie}></div>
-
-          {[1, 2, 3, 4].map((item) => (
-            <div
-              key={item}
-              className={styles.legend}
-            ></div>
-          ))}
-        </div>
-
-        <div className={styles.chartCard}>
-          <div className={styles.heading}></div>
-
-          <div className={styles.barWrapper}>
-            {[1, 2, 3, 4, 5, 6, 7].map((item) => (
-              <div
-                key={item}
-                className={styles.bar}
-              ></div>
-            ))}
+      <div className="row mt-5">
+        <div className="col-md-6">
+          <div className={styles.chartCard}>
+            <div className={styles.chartHeading}></div>
+            <div className={styles.chart}></div>
           </div>
         </div>
 
+        <div className="col-md-6">
+          <div className={styles.chartCard}>
+            <div className={styles.chartHeading}></div>
+            <div className={styles.chart}></div>
+          </div>
+        </div>
       </div>
 
       {/* Table */}
       <div className={styles.tableCard}>
+        <div className={styles.tableHeading}></div>
 
-        <div className={styles.heading}></div>
+        <div className={styles.tableHeader}></div>
 
         {[1, 2, 3, 4, 5].map((item) => (
-          <div
-            key={item}
-            className={styles.tableRow}
-          ></div>
+          <div key={item} className={styles.tableRow}></div>
         ))}
 
+        {/* Pagination */}
+        <div className={styles.pagination}></div>
       </div>
-
     </div>
   );
 }
